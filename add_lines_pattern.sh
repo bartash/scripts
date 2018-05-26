d365ba23848486792133a1647c6fca4540a1f941
Andrew Sherman <andrew@ohair-sherman.com>
2018-05-25 17:35:55 -0700

    loop over files in xref.all, making edit

--- add_lines_pattern.sh
+++ add_lines_pattern.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 # add some lines after matching a pattern
-gsed -i '/	numFiles/a\\tnumFilesErasureCoded	0                   ' $1
+TMP=/tmp/add_lines$$
+grep .q.out  xref.all | xargs grep -l '\tnumFiles' > $TMP
+wc -l $TMP
+for file in `cat $TMP`
+do
+    gsed -i '/	numFiles/a\\tnumFilesErasureCoded	0                   ' $file
+done
