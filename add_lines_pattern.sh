8e862188993b912bba69cad563ba9966d13dea48
Andrew Sherman <andrew@ohair-sherman.com>
2018-05-29 10:38:34 -0700

    allow input fil to be am optional  parameter

--- add_lines_pattern.sh
+++ add_lines_pattern.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # add some lines after matching a pattern
+INPUT=xref.all
+if [ "$#" -eq 1 ]
+then
+  INPUT=$1
+fi
 TMP=/tmp/add_lines$$
-grep .q.out  xref.all | xargs grep -l '\tnumFiles' > $TMP
+grep .q.out  $INPUT | xargs grep -l '\tnumFiles' > $TMP
 wc -l $TMP
 for file in `cat $TMP`
 do
