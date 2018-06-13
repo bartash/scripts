d996d61a36efef38ccfbe2e7db6a694b36d2ebbc
Andrew Sherman <andrew@ohair-sherman.com>
2018-06-13 15:03:28 -0700

    Source a common script to set REPO_NAMES to a list of repositories to to look at

---
+++ repos.sh
@@ -0,0 +1,20 @@
+# set REPO_NAMES to a list of repositories to to look at
+REPO_NAMES=( \
+    asf/hadoop.git  \
+    asf/hive-iml.git \
+    asf/hive.git  \
+    asf/hive2.git  \
+    cdh/asherman-jenkins-master.git \
+    cdh/hadoop.git \
+    cdh/hive-iml.git \
+    cdh/hive.git \
+    cdh/hive2-iml.git \
+    cdh/hive2.git \
+    cdh/hive3.git \
+    jdbc.git \
+    kitchen/cloudcat-hive-ptest.git \
+    kitchen/cloudstack-reporting.git \
+    scripts.git \
+    ssh.git \
+    starship/cmf.git \
+    )
