293e94540e4572295ddefa8d52a5db9f87e1424e
Andrew Sherman <andrew@ohair-sherman.com>
2018-06-19 17:12:10 -0700

    put most important repos first

--- repos.sh
+++ repos.sh
@@ -1,16 +1,16 @@
 # set REPO_NAMES to a list of repositories to to look at
 REPO_NAMES=( \
-    asf/hadoop.git  \
-    asf/hive-iml.git \
     asf/hive.git  \
     asf/hive2.git  \
+    cdh/hive.git \
+    cdh/hive2.git \
+    cdh/hive3.git \
+    asf/hadoop.git  \
+    asf/hive-iml.git \
     cdh/asherman-jenkins-master.git \
     cdh/hadoop.git \
     cdh/hive-iml.git \
-    cdh/hive.git \
     cdh/hive2-iml.git \
-    cdh/hive2.git \
-    cdh/hive3.git \
     jdbc.git \
     kitchen/cloudcat-hive-ptest.git \
     kitchen/cloudstack-reporting.git \
