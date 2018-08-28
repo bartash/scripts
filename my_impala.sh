1574c9dad6709325779a0b434efa67a5aa8511f5
Andrew Sherman <asherman@cloudera.com>
2018-08-28 16:19:40 -0700

    start to allow for multiple work dirs

---
+++ my_impala.sh
@@ -0,0 +1,14 @@
+if [ -z "$IMPALA_HOME" ]; then
+    echo "Need to set IMPALA_HOME"
+    return 1
+fi  
+
+AAIMPALA_BIN=${IMPALA_HOME}/bin
+if [ -f "${AAIMPALA_BIN}/impala-config.sh" ]; then
+    . ${AAIMPALA_BIN}/impala-config.sh
+    export PATH="${AAIMPALA_BIN}:$PATH"
+fi
+# start hadoop services
+
+alias run-all='$IMPALA_HOME/testdata/bin/run-all.sh'
+alias kill-all='$IMPALA_HOME/testdata/bin/kill-all.sh'
