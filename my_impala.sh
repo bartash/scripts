0ab63cee7d2a550661b09cd75f6cce0d0141d6d4
Andrew Sherman <asherman@cloudera.com>
2018-08-28 16:35:18 -0700

    unset some vars so that they get picked up in impala-config.sh

--- my_impala.sh
+++ my_impala.sh
@@ -2,6 +2,13 @@ if [ -z "$IMPALA_HOME" ]; then
     echo "Need to set IMPALA_HOME"
     return 1
 fi  
+unset IMPALA_TOOLCHAIN
+unset IMPALA_LOGS_DIR
+unset IMPALA_AUX_TEST_HOME
+unset HADOOP_LZO
+unset IMPALA_LZO
+unset METASTORE_DB
+unset SENTRY_POLICY_DB
 
 AAIMPALA_BIN=${IMPALA_HOME}/bin
 if [ -f "${AAIMPALA_BIN}/impala-config.sh" ]; then
