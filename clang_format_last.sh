98e05235dd05a3237d6b4bfd92b418d595f2c349
Andrew Sherman <asherman@cloudera.com>
2019-01-02 14:30:16 -0800

    By default we use the last change, but use first arg if it is there

--- clang_format_last.sh
+++ clang_format_last.sh
@@ -6,7 +6,14 @@ then
   exit 1
 fi
 
+# by default we use the last change, but use first arg if it is there
+AARG="HEAD"
+if [ "$#" -eq 1 ]
+then
+	AARG=$1
+fi
+
 # see https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=65868536
-git diff -U0 --no-color HEAD^ | "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/share/clang/clang-format-diff.py" -binary "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/bin/clang-format" -p1 -i
+git diff -U0 --no-color "$AARG"^ | "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/share/clang/clang-format-diff.py" -binary "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/bin/clang-format" -p1 -i -v
 
 print_long_lines_in_commit
