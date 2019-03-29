b746f3c1a093035b884ffda139d69ae8267aa97d
Andrew Sherman <asherman@cloudera.com>
2019-03-29 13:32:27 -0700

    wrap lines for clarity

--- clang_format_last.sh
+++ clang_format_last.sh
@@ -14,6 +14,9 @@ then
 fi
 
 # see https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=65868536
-git diff -U0 --no-color "$AARG"^ | "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/share/clang/clang-format-diff.py" -binary "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/bin/clang-format" -style=file -p1 -i -v
+git diff -U0 --no-color "$AARG"^ | \
+	"${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/share/clang/clang-format-diff.py" \
+	-binary "${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/bin/clang-format" \
+	-style=file -p1 -i -v
 
 print_long_lines_in_commit
