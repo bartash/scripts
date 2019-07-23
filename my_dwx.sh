5854e58e31d9bfc74fcc28581c151d21a1615670
Andrew Sherman <asherman@cloudera.com>
2019-07-22 17:44:31 -0700

    tidy

--- my_dwx.sh
+++ my_dwx.sh
@@ -3,14 +3,13 @@
 if [ -z "$AADWX" ]; then
     echo "Need to set AADWX"
     return 1
-fi  
+fi
 
 export GOPATH=$HOME/git/go
-export GO111MODULE=on 
+export GO111MODULE=on
 export DWX_HOME=$GOPATH/src/github.com/hortonworks/edws
 . ~/scripts/gosetup.sh
 export PATH=$PATH:/home/asherman/git/asf/Impala3/toolchain/cdh_components-1137441/hadoop-3.0.0-cdh6.x-SNAPSHOT/bin
 
 . ~/Dropbox/work/cloudera/${AADWX}_env.sh
-alias ihome="cd $DWX_HOME" 
-
+alias ihome="cd $DWX_HOME"
