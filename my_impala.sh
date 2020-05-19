1b9bc99ec8933816f479261b16ebb30da2c84130
Andrew Sherman <asherman@cloudera.com>
2020-05-19 08:38:23 -0700

    update build farm (not 100% tested?)

--- my_impala.sh
+++ my_impala.sh
@@ -34,7 +34,8 @@ asherman-desktop*)
   # https://cloudera.atlassian.net/wiki/spaces/ENG/pages/100832437/Tips+for+Faster+Impala+Builds
   # export BUILD_FARM="vc0332.halxg.cloudera.com/96,lzo vc0336.halxg.cloudera.com/96,lzo 172.16.34.252/16,lzo impala-desktop.pa.cloudera.com/16,lzo impala-distcc-server.gce.cloudera.com/16,lzo 172.16.34.44/8,lzo"
   # export BUILD_FARM="vc0332.halxg.cloudera.com/96,lzo vc0336.halxg.cloudera.com/96,lzo 172.16.34.252/16,lzo impala-desktop.pa.cloudera.com/16,lzo 172.16.34.44/8,lzo"
-  export BUILD_FARM="vc0332.halxg.cloudera.com/96,lzo vc0336.halxg.cloudera.com/96,lzo 172.16.34.252/16,lzo impala-desktop.pa.cloudera.com/16,lzo 172.16.34.44/8,lzo ccycloud.impala-distcc.root.hwx.site/48,lzo"
+  # export BUILD_FARM="vc0332.halxg.cloudera.com/96,lzo vc0336.halxg.cloudera.com/96,lzo 172.16.34.252/16,lzo impala-desktop.pa.cloudera.com/16,lzo 172.16.34.44/8,lzo ccycloud.impala-distcc.root.hwx.site/48,lzo"
+  export BUILD_FARM="vc0332.halxg.cloudera.com/96,lzo vc0336.halxg.cloudera.com/96,lzo impala-desktop.pa.cloudera.com/16,lzo ccycloud.impala-distcc.root.hwx.site/48,lzo"
   source "${IMPALA_HOME}"/bin/distcc/distcc_env.sh
   # May be required to update the build parallelism if you changed BUILD_FARM.
   switch_compiler distcc
