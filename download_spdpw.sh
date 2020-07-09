ce0ccf0e61aadfe226c78b7544171a1326695c33
Andrew Sherman <asherman@cloudera.com>
2020-07-09 08:58:03 -0700

    wait 1 sec between requests

--- download_spdpw.sh
+++ download_spdpw.sh
@@ -1,2 +1,2 @@
 # mirror site
-wget -r --reject tif,TIF,cal,CAL,lnk  -nc --limit-rate=50k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
+wget -r --wait=1 --reject tif,TIF,cal,CAL,lnk  -nc --limit-rate=50k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
