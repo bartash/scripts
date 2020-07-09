71f46c5c1893ea6adaa91078eaca10a931aa96b7
Andrew Sherman <asherman@cloudera.com>
2020-07-09 08:54:56 -0700

    rm dup, try to exlude tif

--- download_spdpw.sh
+++ download_spdpw.sh
@@ -1,2 +1,2 @@
 # mirror site
-wget -r  -nc --limit-rate=50k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
+wget -r --reject tif,TIF,cal,CAL,lnk  -nc --limit-rate=50k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
--- sync_dpw.sh
+++
@@ -1,2 +0,0 @@
-#
-wget -r  --limit-rate=50k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
