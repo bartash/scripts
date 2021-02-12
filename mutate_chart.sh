8ff8874c7c305f7e0df4baa306c649865303c8e9
Andrew Sherman <asherman@cloudera.com>
2021-02-11 17:00:15 -0800

    first cut at mutate_chart.sh

---
+++ mutate_chart.sh
@@ -0,0 +1,28 @@
+#
+# Edit a chart ot make it helm3 compatible
+#
+
+if [ "$#" -ne 1 ]
+then
+ echo "usage $0 <chart tgz file>"
+ exit 1
+fi
+
+ORIG=$1
+
+if [ ! -f $ORIG ] ; then
+  echo "file $ORIG not found"
+  exit 1
+fi
+
+
+NEWFILE=$(echo $ORIG | sed 's/.tgz/-helm3.tgz')
+
+echo ORIG=$ORIG
+echo NEWFILE=$NEWFILE
+
+DIR=impala-coordinator
+
+rm -rf $DIR
+tar xvf $ORIG
+tar cvzf  $NEWFILE  $DIR
