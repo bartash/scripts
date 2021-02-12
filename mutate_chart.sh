880acfdf491693bc5a2dc25e0b007465adef8a9a
Andrew Sherman <asherman@cloudera.com>
2021-02-11 17:20:47 -0800

    fixes

--- mutate_chart.sh
+++ mutate_chart.sh
@@ -7,24 +7,19 @@ then
  echo "usage $0 <chart tgz file>"
  exit 1
 fi
-
 ORIG=$1
-
 if [ ! -f $ORIG ] ; then
   echo "file $ORIG not found"
   exit 1
 fi
 
-
 NEWFILE=$(echo $ORIG | sed 's/.tgz/-helm3.tgz/')
-
-echo ORIG=$ORIG
-echo NEWFILE=$NEWFILE
-
+#echo ORIG=$ORIG
+#echo NEWFILE=$NEWFILE
 DIR=impala-coordinator
-
 rm -rf $DIR
-echo "extract from $ORIG"
-tar xvf $ORIG
+
+echo "Extract from $ORIG"
+tar xf $ORIG
 echo "Write $DIR into $NEWFILE"
-tar cvzf  $NEWFILE  $DIR
+tar czf  $NEWFILE  $DIR
