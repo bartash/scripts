d69aae70ba7c0508ab293ccef20cbb5be911bf0f
Andrew Sherman <asherman@cloudera.com>
2021-05-17 12:10:55 -0700

    tidy

--- resolve_templates.sh
+++ resolve_templates.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # run helm template on chart
 #
@@ -6,8 +7,10 @@ rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 cp -r charts/impala-coordinator ${BUILD_DIR}
 rm -rf ${BUILD_DIR}/impala-coordinator/charts/dwx-common
+# remove reference to dwx-common from Chart.yaml
 TMP_FILE=/tmp/foo$$
 cp ${BUILD_DIR}/impala-coordinator/Chart.yaml ${TMP_FILE}
 grep -v dwx-common ${TMP_FILE} >${BUILD_DIR}/impala-coordinator/Chart.yaml
+# build .tgz file
 tar -C ${BUILD_DIR} -czf /tmp/x.tar.tgz impala-coordinator
 helm3 template /tmp/x.tar.tgz
