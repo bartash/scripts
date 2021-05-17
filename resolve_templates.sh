cb918ab1a45d0c01b6f5f7fc8a84cf33eac0782f
Andrew Sherman <asherman@cloudera.com>
2021-05-17 12:05:59 -0700

    tidy script to run helm template on chart

--- resolve_templates.sh
+++ resolve_templates.sh
@@ -1,12 +1,13 @@
 #
-# run helm templatye on chart
+# run helm template on chart
 #
-rm -rf /tmp/chart_build
-mkdir /tmp/chart_build
-cp -r charts/impala-coordinator /tmp/chart_build
-# cp -r charts/dwx-common /tmp/chart_build
-rm -rf /tmp/chart_build/impala-coordinator/charts/dwx-common
-cp /tmp/chart_build/impala-coordinator/Chart.yaml /tmp/foo
-grep -v dwx-common /tmp/foo > /tmp/chart_build/impala-coordinator/Chart.yaml
-tar -C /tmp/chart_build   -czf /tmp/x.tar.tgz impala-coordinator
+BUILD_DIR=/tmp/chart_build
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+cp -r charts/impala-coordinator ${BUILD_DIR}
+rm -rf ${BUILD_DIR}/impala-coordinator/charts/dwx-common
+TMP_FILE=/tmp/foo$$
+cp ${BUILD_DIR}/impala-coordinator/Chart.yaml ${TMP_FILE}
+grep -v dwx-common ${TMP_FILE} >${BUILD_DIR}/impala-coordinator/Chart.yaml
+tar -C ${BUILD_DIR} -czf /tmp/x.tar.tgz impala-coordinator
 helm3 template /tmp/x.tar.tgz
