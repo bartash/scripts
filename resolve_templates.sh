d28c74362856983ba2f6539432a98592a1979a7f
Andrew Sherman <asherman@cloudera.com>
2021-05-17 12:03:05 -0700

    better  resolve_templates.sh

--- resolve_templates.sh
+++ resolve_templates.sh
@@ -1,5 +1,12 @@
+#
+# run helm templatye on chart
+#
 rm -rf /tmp/chart_build
 mkdir /tmp/chart_build
-cp -r charts/dwx-common /tmp/chart_build
 cp -r charts/impala-coordinator /tmp/chart_build
+# cp -r charts/dwx-common /tmp/chart_build
+rm -rf /tmp/chart_build/impala-coordinator/charts/dwx-common
+cp /tmp/chart_build/impala-coordinator/Chart.yaml /tmp/foo
+grep -v dwx-common /tmp/foo > /tmp/chart_build/impala-coordinator/Chart.yaml
 tar -C /tmp/chart_build   -czf /tmp/x.tar.tgz impala-coordinator
+helm3 template /tmp/x.tar.tgz
