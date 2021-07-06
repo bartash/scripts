5360110c104dfd37c909fd66511fb91027c5681a
Andrew Sherman <asherman@cloudera.com>
2021-07-05 19:04:29 -0700

    skip more file extensions, throttle at 25k

--- download_spdpw.sh
+++ download_spdpw.sh
@@ -1,2 +1,3 @@
 # mirror site
-wget -r --wait=1 --reject tif,TIF,cal,CAL,lnk  -nc --limit-rate=50k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
+cd ~/Amy
+wget -r --wait=1 --reject DWG,aspx,vb,sid,err,G4,TBK,g4,GP4,ovr,TFw,FXP,DBF,PRG,IDX,FPT,dwg,bak,tfw,tif,TIF,cal,CAL,lnk  -nc --limit-rate=25k 'http://bsmnt.sfdpw.org/subdmap/subd/State%20Highway%20Map/Blueprints/'
