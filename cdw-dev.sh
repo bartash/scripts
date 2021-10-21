2817935a4f52780f6fa930c1378100f2d373b6ce
Andrew Sherman <asherman@cloudera.com>
2021-10-21 15:13:45 -0700

    remove quotes

--- cdw-dev.sh
+++ cdw-dev.sh
@@ -1,5 +1,5 @@
 # Some values from https://docs.google.com/document/d/16ySv2fdWdXIABpj5VIgbjD0preIO9CY-pgk0qUr7p0c
-export KUBECONFIG="~/.kube/cdw-dev-kubeconfig"
+export KUBECONFIG=~/.kube/cdw-dev-kubeconfig
 export SDX_CDP_CONSOLE_HOST_URL="https://console-cdp-acsertan.apps.cdw-dev.kcloud.cloudera.com"
 export CSI_NAMESPACE="cdp-acsertan"
 export OPENSHIFT_NAMESPACE_PREFIX="cdp-acsertan-env-8fb66bab"
