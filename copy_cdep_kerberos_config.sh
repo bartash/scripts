fdc0179efe850c485502778c90e7c1c4b3567f0c
Andrew Sherman <asherman@cloudera.com>
2021-11-04 12:58:43 -0700

    add klist

--- copy_cdep_kerberos_config.sh
+++ copy_cdep_kerberos_config.sh
@@ -23,8 +23,11 @@ echo "Overwriting /etc/krb5.conf"
 sudo cp krb5.conf /etc/krb5.conf
 echo "Copying certificate"
 scp -r  -i ~/.ssh/systest_rsa  systest@${HOST}:/var/lib/cloudera-scm-agent/agent-cert/cm-auto-global_cacerts.pem .
-echo "Running kinit for systest"
+echo "Running 'kinit -kt cdep/keytabs/systest.keytab systest'"
 kinit -kt cdep/keytabs/systest.keytab systest
+
+echo "klist output:"
+klist
 
 echo "Impala-shell string"
 echo "impala-shell -i ${HOST} -d default -k --ssl --ca_cert=${CONFIG}/cm-auto-global_cacerts.pem"
