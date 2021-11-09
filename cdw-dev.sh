03264ea681dfa0550d430c46f2956e70f334ebd2
Andrew Sherman <asherman@cloudera.com>
2021-11-09 11:15:18 -0800

    How to get DEBUG_ACTOR_CRN

--- cdw-dev.sh
+++ cdw-dev.sh
@@ -3,6 +3,8 @@ export KUBECONFIG=~/.kube/cdw-dev-kubeconfig
 export SDX_CDP_CONSOLE_HOST_URL="https://console-asherman2.apps.cdw-dev.kcloud.cloudera.com/"
 export CSI_NAMESPACE="asherman2"
 export OPENSHIFT_NAMESPACE_PREFIX="asherman2-env-1-f85e7a62"
-# DEBUG_ACTOR_CRN comes by looking in the json returned from the environments rest call
-export DEBUG_ACTOR_CRN="crn:altus:iam:us-west-1:5ee9e40c-d041-40f3-9225-451fa4281c3f:user:c4931fae-cb25-40b2-8709-f4bbdc3e5865"
+# DEBUG_ACTOR_CRN comes from the cdp-pvt-session-token cookie
+# chrome browser tools->Application->Cookies
+# Paste it to https://jwt.io/ to decode
+export DEBUG_ACTOR_CRN="crn:altus:iam:us-west-1:5f7b4303-cbfe-44e0-86c7-43e074868122:user:8b7c8609-6dc1-4b2d-bd71-d287e3bf1bc9"
 
