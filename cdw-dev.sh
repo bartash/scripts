0b03fc106b5a8b3900725bc34366d68ae4d2a5fa
Andrew Sherman <asherman@cloudera.com>
2021-11-09 10:21:03 -0800

    I *think* these vales actually come from the env that I made

--- cdw-dev.sh
+++ cdw-dev.sh
@@ -1,7 +1,8 @@
-# Some values from https://docs.google.com/document/d/16ySv2fdWdXIABpj5VIgbjD0preIO9CY-pgk0qUr7p0c
+# Some values from a recent env
 export KUBECONFIG=~/.kube/cdw-dev-kubeconfig
-export SDX_CDP_CONSOLE_HOST_URL="https://console-cdp-acsertan.apps.cdw-dev.kcloud.cloudera.com"
-export CSI_NAMESPACE="cdp-acsertan"
-export OPENSHIFT_NAMESPACE_PREFIX="cdp-acsertan-env-8fb66bab"
+export SDX_CDP_CONSOLE_HOST_URL="https://console-asherman2.apps.cdw-dev.kcloud.cloudera.com/"
+export CSI_NAMESPACE="asherman2"
+export OPENSHIFT_NAMESPACE_PREFIX="asherman2-env-1-f85e7a62"
+# DEBUG_ACTOR_CRN comes by looking in the json returned from the environments rest call
 export DEBUG_ACTOR_CRN="crn:altus:iam:us-west-1:5ee9e40c-d041-40f3-9225-451fa4281c3f:user:c4931fae-cb25-40b2-8709-f4bbdc3e5865"
 
