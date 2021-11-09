14b16ed248b3872cb21869f97cfc08cbe557d062
Andrew Sherman <asherman@cloudera.com>
2021-11-08 17:30:20 -0800

    add another value from https://docs.google.com/document/d/16ySv2fdWdXIABpj5VIgbjD0preIO9CY-pgk0qUr7p0c

--- cdw-dev.sh
+++ cdw-dev.sh
@@ -3,4 +3,5 @@ export KUBECONFIG=~/.kube/cdw-dev-kubeconfig
 export SDX_CDP_CONSOLE_HOST_URL="https://console-cdp-acsertan.apps.cdw-dev.kcloud.cloudera.com"
 export CSI_NAMESPACE="cdp-acsertan"
 export OPENSHIFT_NAMESPACE_PREFIX="cdp-acsertan-env-8fb66bab"
+export DEBUG_ACTOR_CRN="crn:altus:iam:us-west-1:5ee9e40c-d041-40f3-9225-451fa4281c3f:user:c4931fae-cb25-40b2-8709-f4bbdc3e5865"
 
