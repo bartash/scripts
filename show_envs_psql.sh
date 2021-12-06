953b8d8e4c6a588aa5bf345ab71786a6373531ba
Andrew Sherman <asherman@cloudera.com>
2021-12-06 11:47:52 -0800

    save link for dig to julia evans blog

--- show_envs_psql.sh
+++ show_envs_psql.sh
@@ -1,2 +1,2 @@
 # show all the ids  from edws_cluster in the db
-echo 'select id, environment_id from edws_cluster' | psql -t -q -U postgres
+echo 'select id, environment_id, account_id, cloud_platform, created_by from edws_cluster' | psql -t -q -U postgres
