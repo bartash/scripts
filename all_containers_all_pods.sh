e5932a09c5cf75764eca04137a8c410da991e061
Andrew Sherman <asherman@cloudera.com>
2021-12-11 11:39:25 -0800

    Search for log4j files in all containers of all pods

---
+++ all_containers_all_pods.sh
@@ -0,0 +1,22 @@
+#
+# Search for log4j files in all containers of all pods
+#
+export KUBECONFIG=~/.kube/dwx-rzs556
+export NS=compute-1639142720-gvtw  
+# export NS=impala-1639153773-6bfg
+for pod in $(kubectl  get pods -n $NS |  grep -v NAME | awk '{print $1}')
+do 
+	for container in $(kubectl -n $NS get pods $pod -o jsonpath={.spec.containers[*].name})
+	do 
+		echo "$pod $container"
+		TMP=/tmp/xx-$pod-$container
+		kubectl -n $NS exec -it $pod -c $container  -- ls -R /  > $TMP
+		grep -i log4j $TMP
+	done
+	# init containers
+	for init_container in $(kubectl -n $NS get pods $pod -o jsonpath={.spec.initContainers[*].name})
+	do 
+		echo "ignoring $pod $init_container (init container)"
+	done
+done
+
