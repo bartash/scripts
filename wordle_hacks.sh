18678fc9be379d3bafbda4ed14182936cc3bf8b7
Andrew Sherman <asherman@cloudera.com>
2022-01-07 08:52:32 -0800

    Brute force hack to find opening word pairs for wordle that cover etaionshrd

---
+++ wordle_hacks.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Brute force hack to find opening word pairs for wordle that cover etaionshrd
+# e.g. "adore" "hints"
+#
+COMMON=/tmp/comm$$
+CROSS=/tmp/cross$$
+ET='[etaionshrd]'
+egrep "^${ET}${ET}${ET}${ET}${ET}$" /usr/share/dict/american-english > $COMMON
+for i in $(cat $COMMON)
+do
+	for j in $(cat $COMMON)
+	do
+		echo $i$j >> $CROSS
+	done
+done
+
+for word in $(cat $CROSS)
+do
+  count=$(echo $word | sed 's/\(.\)/\1\n/g'  | sort -u | wc -l)
+  if [[ "$count" == "11" ]]
+  then
+    echo ${word:0:5}  ${word:5:10}
+  fi
+done
