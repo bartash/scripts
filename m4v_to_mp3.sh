372dd47553da389dd11f67da0ce6961f1a42ab3f
Andrew Sherman <asherman@cloudera.com>
2022-01-25 09:15:19 -0800

    wip

--- m4v_to_mp3.sh
+++ m4v_to_mp3.sh
@@ -2,6 +2,9 @@
 # convert ripped dvd into mp3
 
 find . -type f -name "*.m4v" -print0 | while IFS= read -r -d '' file; do
-    echo "file = '$file'"
+	echo "file = '$file'"
+	new=$(echo $file | sed 's/.m4v/.mp3/' | sed 's?./??')
+	echo new=$new
+	echo ffmpeg -i $file -q:a 0 -map a $new
 done
 
