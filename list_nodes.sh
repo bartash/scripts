95b4179ba80e964986df3fbfb1073575ef7f5205
Andrew Sherman <asherman@cloudera.com>
2022-01-28 18:22:08 -0800

    print pods and namespaces for each node

--- list_nodes.sh
+++ list_nodes.sh
@@ -5,7 +5,8 @@ NODES=/tmp/nd$$
 kubectl get nodes --no-headers  | awk '{print $1}' > $NODES
 for node in $(cat $NODES)
 do
-  echo $node
-  kubectl get pods --all-namespaces  --field-selector spec.nodeName=$node | grep -v ^cluster | grep -v ^kube-system
+  echo "NODE $node"
+  kubectl get pods --all-namespaces  --field-selector spec.nodeName=$node | grep -v ^cluster | grep -v ^kube-system |\
+  awk '{printf "%-55s %-20s\n", $2, $1}'
   echo "-----------"
 done
