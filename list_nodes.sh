52ef2e6090e63ce7c4c9934a2283c884e4f42aff
Andrew Sherman <asherman@cloudera.com>
2022-01-31 15:55:34 -0800

    add provider id to nodes list

--- list_nodes.sh
+++ list_nodes.sh
@@ -5,7 +5,8 @@ NODES=/tmp/nd$$
 kubectl get nodes --no-headers  | awk '{print $1}' > $NODES
 for node in $(cat $NODES)
 do
-  echo "NODE $node"
+  id=$(kubectl describe node $node | grep ProviderID: | awk '{print $2}')
+  echo "NODE $node $id"
   kubectl get pods --all-namespaces  --field-selector spec.nodeName=$node | grep -v ^cluster | grep -v ^kube-system |\
   awk '{printf "%-55s %-20s\n", $2, $1}'
   echo "-----------"
