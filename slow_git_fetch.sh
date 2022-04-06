4448ff0a384597e3260bd7bda88516a823adab53
Andrew Sherman <asherman@cloudera.com>
2022-04-06 08:38:00 -0700

    add rmeote param

--- slow_git_fetch.sh
+++ slow_git_fetch.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # gradually fetch the rest of a repo, maybe created with 'git clone --depth=1'
 
+REMOTE=origin
+if [[ $# -ne 0 ]] ; then
+    REMOTE=$1
+fi
 # edit depth when you start FIXME add parameter
 depth=6000
 while true
 do
   depth=$((depth + 100))
   echo "depth is $depth"
-  git fetch --depth $depth
+  git fetch --depth $depth $REMOTE
   sleep 10
 done
