2824f108c9bb3cf0ba0c9ae156886feb39d1e48d
Andrew Sherman <asherman@cloudera.com>
2022-04-08 08:30:45 -0700

    save

--- shared-os-dev-01.sh
+++ shared-os-dev-01.sh
@@ -1,1 +1,3 @@
 export VAULT_ADDR="https://vault-shared-os-dev-01-control-plane-vault.apps.shared-os-dev-01.kcloud.cloudera.com"
+export VAULT_SKIP_VERIFY="True"
+vault login -method=ldap username=asherman@cloudera.com
