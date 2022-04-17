96c207ac2198ed10cba9cf88fdba8d1d9a8dd54c
Andrew Sherman <asherman@cloudera.com>
2022-04-17 13:45:29 -0700

    update OPENSHIFT_NAMESPACE_PREFIX

--- cdw-dev1.sh
+++ cdw-dev1.sh
@@ -59,7 +59,7 @@ export OPENSHIFT_DB_PASSWORD=QnBMbmZnRHNjMldEOEYy
 # Used for finding the $OPENSHIFT_NAMESPACE_PREFIX-monitoring-platform
 # namespace which is created during environment registration in
 # the CDP control plane
-export OPENSHIFT_NAMESPACE_PREFIX=asherman-env-1-5784c541-monitoring-platform
+export OPENSHIFT_NAMESPACE_PREFIX=asherman2-env-1-29106b0d-monitoring-platform
 
 # User Management Service related settings
 # See this section on how to get the CRN or copy from here if
