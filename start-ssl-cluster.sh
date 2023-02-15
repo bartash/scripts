47bd3dad55eefb2b9a7921d47216ca4ed0b39b34
Andrew Sherman <asherman@cloudera.com>
2023-02-15 14:34:28 -0800

    WIP start-ssl params

--- start-ssl-cluster.sh
+++ start-ssl-cluster.sh
@@ -1,9 +1,13 @@
-
+#!/usr/bin/env bash
 #
 # start impala cluster with ssl
+#
 if [ -z "$IMPALA_HOME" ]; then
 	echo "IMPALA_HOME not set"
 	exit 2;
 fi
 
-SSL_ARGS="--ssl_client_ca_certificate=$IMPALA_HOME/be/src/testutil/server-cert.pem --ssl_server_certificate=$IMPALA_HOME/be/src/testutil/server-cert.pem --ssl_private_key=$IMPALA_HOME/be/src/testutil/server-key.pem --hostname=localhost"
+SSL_ARGS="--ssl_client_ca_certificate=${IMPALA_HOME}/be/src/testutil/server-cert.pem --ssl_server_certificate=${IMPALA_HOME}/be/src/testutil/server-cert.pem --ssl_private_key=${IMPALA_HOME}/be/src/testutil/server-key.pem --hostname=localhost"
+# echo "SSL_SRGS=${SSL_ARGS}"
+
+start-impala-cluster.py --cluster_size=1 --impalad_args="${SSL_ARGS}" --catalogd_args="${SSL_ARGS}" --state_store_args="${SSL_ARGS}"
