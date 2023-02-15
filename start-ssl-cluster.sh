4eef43225a14871dc6b9bf89340c8e24e1233f41
Andrew Sherman <asherman@cloudera.com>
2023-02-15 14:47:31 -0800

    split lines, add webserver

--- start-ssl-cluster.sh
+++ start-ssl-cluster.sh
@@ -7,7 +7,17 @@ if [ -z "$IMPALA_HOME" ]; then
 	exit 2;
 fi
 
-SSL_ARGS="--ssl_client_ca_certificate=${IMPALA_HOME}/be/src/testutil/server-cert.pem --ssl_server_certificate=${IMPALA_HOME}/be/src/testutil/server-cert.pem --ssl_private_key=${IMPALA_HOME}/be/src/testutil/server-key.pem --hostname=localhost"
+SSL_ARGS="--ssl_client_ca_certificate=${IMPALA_HOME}/be/src/testutil/server-cert.pem \
+--ssl_server_certificate=${IMPALA_HOME}/be/src/testutil/server-cert.pem \
+--ssl_private_key=${IMPALA_HOME}/be/src/testutil/server-key.pem \
+--webserver_certificate_file=${IMPALA_HOME}/be/src/testutil/server-cert.pem \
+--webserver_private_key_file=${IMPALA_HOME}/be/src/testutil/server-key.pem \
+--hostname=localhost \
+--webserver_interface=localhost "
 # echo "SSL_SRGS=${SSL_ARGS}"
 
-start-impala-cluster.py --cluster_size=1 --impalad_args="${SSL_ARGS}" --catalogd_args="${SSL_ARGS}" --state_store_args="${SSL_ARGS}"
+start-impala-cluster.py --cluster_size=1 --impalad_args="${SSL_ARGS}" \
+ --catalogd_args="${SSL_ARGS}" \
+  --state_store_args="${SSL_ARGS}" \
+
+
