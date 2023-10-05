3c2845be8f3508f9ac0569f36889b433c82c81b0
Andrew Sherman <asherman@cloudera.com>
2023-10-05 11:35:29 -0700

    tidy up find_fastest_to_100_commits.sh

--- find_fastest_to_100_commits.sh
+++ find_fastest_to_100_commits.sh
@@ -2,21 +2,27 @@
 #
 # Find how many days it took each contributor to reach 100 commits
 
+# number of commits
+COMMITS=100
+DAYS_IN_MONTH=30
+MONTHS_IN_YEAR=12
 # shellcheck disable=SC2066
 IFS=
 # I know there are 33 people with 100 commits
 git shortlog -s -n  | head -33  | cut -c8- | while read -r user
 do
-  start=$(git log --pretty=format:"%ad" --date=format:'%Y-%m-%d'  --author="$user" | tail -100 | tail -1)
-  start_year=$(git log --pretty=format:"%ad" --date=format:'%Y'  --author="$user" | tail -100 | tail -1)
-  start_month=$(git log --pretty=format:"%ad" --date=format:'%m'  --author="$user" | tail -100 | tail -1)
-  start_days=$(git log --pretty=format:"%ad" --date=format:'%d'  --author="$user" | tail -100 | tail -1)
-  start_total=$(echo "((($start_year * 12) + $start_month) * 30) + $start_days" | bc)
-  end=$(git log --pretty=format:"%ad" --date=format:'%Y-%m-%d'  --author="$user" | tail -100 | head -1)
-  end_year=$(git log --pretty=format:"%ad" --date=format:'%Y'  --author="$user" | tail -100 | head -1)
-  end_month=$(git log --pretty=format:"%ad" --date=format:'%m'  --author="$user" | tail -100 | head -1)
-  end_days=$(git log --pretty=format:"%ad" --date=format:'%d'  --author="$user" | tail -100 | head -1)
-  end_total=$(echo "((($end_year * 12) + $end_month) * 30) + $end_days" | bc)
+  start=$(git log --pretty=format:"%ad" --date=format:'%Y-%m-%d'  --author="$user" | tail -"$COMMITS" | tail -1)
+  start_year=$(git log --pretty=format:"%ad" --date=format:'%Y'   --author="$user" | tail  -"$COMMITS"  | tail -1)
+  start_month=$(git log --pretty=format:"%ad" --date=format:'%m'  --author="$user" | tail -"$COMMITS"  | tail -1)
+  start_days=$(git log --pretty=format:"%ad" --date=format:'%d'   --author="$user" | tail  -"$COMMITS" | tail -1)
+  start_total=$(echo "((($start_year * $MONTHS_IN_YEAR) + $start_month) * $DAYS_IN_MONTH) + $start_days" | bc)
+
+  end=$(git log --pretty=format:"%ad" --date=format:'%Y-%m-%d'  --author="$user" | tail -"$COMMITS"  | head -1)
+  end_year=$(git log --pretty=format:"%ad" --date=format:'%Y'   --author="$user" | tail  -"$COMMITS"  | head -1)
+  end_month=$(git log --pretty=format:"%ad" --date=format:'%m'  --author="$user" | tail  -"$COMMITS"  | head -1)
+  end_days=$(git log --pretty=format:"%ad" --date=format:'%d'   --author="$user" | tail  -"$COMMITS" | head -1)
+  end_total=$(echo "((($end_year * $MONTHS_IN_YEAR) + $end_month) * $DAYS_IN_MONTH) + $end_days" | bc)
+
   # compute number of days for 100 commits
   diff_days=$(echo "$end_total - $start_total" | bc)
   echo "$diff_days $user ($start to $end)"
