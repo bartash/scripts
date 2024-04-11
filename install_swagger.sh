3ca0bc23e4fa5d1fd95c230b5adb3636842858ee
Andrew Sherman <asherman@cloudera.com>
2024-04-11 15:28:02 -0700

    don't install go

--- install_swagger.sh
+++ install_swagger.sh
@@ -7,11 +7,6 @@ export SWAGGER_CODEGEN_VERSION=2.2.1
 export SWAGGER_CODEGEN_JAR=/usr/local/share/swagger-codegen.jar
 
 
-cd /tmp && \
-    curl -sSL -O https://dl.google.com/go/go${GO_VERSION}.${TARGET_OS}-${TARGET_ARCH}.tar.gz && \
-    tar -xf go${GO_VERSION}.${TARGET_OS}-${TARGET_ARCH}.tar.gz && \
-    rm go${GO_VERSION}.${TARGET_OS}-${TARGET_ARCH}.tar.gz && \
-    mv go /usr/local
 
 curl -sSL \
     -o ${SWAGGER_CODEGEN_JAR} \
