62ef7e2f698f1f2a7441076c21e841ffa552df46
Andrew Sherman <asherman@cloudera.com>
2024-04-18 13:10:53 -0700

    print what is happening

--- clean_mold.sh
+++ clean_mold.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-# clean files left by mold linker
-find | egrep /.mold-...... | xargs rm
+# clean files left by mold lin ker
+TMP=/tmp/clean_mold$$
+find | egrep /.mold-...... > $TMP 
+num=$(cat $TMP | wc -l)
+echo "removing $num mold files"
+if [[ "$num" == "0" ]] ; then
+	echo no files
+	exit 1
+fi
+cat $TMP | xargs rm
