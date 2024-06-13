3ee127b424d36f78a96cdee0977db6e55b353015
Andrew Sherman <asherman@cloudera.com>
2024-06-13 11:00:30 -0700

    save

---
+++ get_white_noise.sh
@@ -0,0 +1,62 @@
+#
+END=51
+
+# dpwnload source into foo of
+# https://mc2method.org/white-noise/
+# grep length=10 foo | sed 's/.*file=//g' | sed 's/\&.*//g' | sort -n
+for i in $(seq 1 $END)
+do
+	echo https://mc2method.org/white-noise/download.php?file=24-Storm&length=10
+
+	echo $i
+done
+https://mc2method.org/white-noise/download.php?file=01-White-Noise&length=10
+https://mc2method.org/white-noise/download.php?file=02-White-Noise&length=10
+https://mc2method.org/white-noise/download.php?file=03-White-Noise&length=10
+https://mc2method.org/white-noise/download.php?file=04-Dryer&length=10
+https://mc2method.org/white-noise/download.php?file=05-Fan&length=10
+https://mc2method.org/white-noise/download.php?file=06-Ocean&length=10
+https://mc2method.org/white-noise/download.php?file=07-PinkNoise&length=10
+https://mc2method.org/white-noise/download.php?file=08-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=09-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=10-Water&length=10
+https://mc2method.org/white-noise/download.php?file=11-Blender&length=10
+https://mc2method.org/white-noise/download.php?file=12-Blender&length=10
+https://mc2method.org/white-noise/download.php?file=14-WaterBoiling&length=10
+https://mc2method.org/white-noise/download.php?file=15-Dryer&length=10
+https://mc2method.org/white-noise/download.php?file=16-Dryer&length=10
+https://mc2method.org/white-noise/download.php?file=17-Fan&length=10
+https://mc2method.org/white-noise/download.php?file=18-Fan&length=10
+https://mc2method.org/white-noise/download.php?file=19-Fan&length=10
+https://mc2method.org/white-noise/download.php?file=20-Fan&length=10
+https://mc2method.org/white-noise/download.php?file=21-Heater&length=10
+https://mc2method.org/white-noise/download.php?file=22-Heater&length=10
+https://mc2method.org/white-noise/download.php?file=23-Heater&length=10
+https://mc2method.org/white-noise/download.php?file=24-Storm&length=10
+https://mc2method.org/white-noise/download.php?file=25-Ocean&length=10
+https://mc2method.org/white-noise/download.php?file=26-Ocean&length=10
+https://mc2method.org/white-noise/download.php?file=27-Shower&length=10
+https://mc2method.org/white-noise/download.php?file=28-Vacuum&length=10
+https://mc2method.org/white-noise/download.php?file=29-WaterBoiling&length=10
+https://mc2method.org/white-noise/download.php?file=30-Stream&length=10
+https://mc2method.org/white-noise/download.php?file=31-Ocean&length=10
+https://mc2method.org/white-noise/download.php?file=32-Waterfall&length=10
+https://mc2method.org/white-noise/download.php?file=33-Fan&length=10
+https://mc2method.org/white-noise/download.php?file=34-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=35-Refrigerator&length=10
+https://mc2method.org/white-noise/download.php?file=36-Train&length=10
+https://mc2method.org/white-noise/download.php?file=37-Ocean&length=10
+https://mc2method.org/white-noise/download.php?file=38-Underwater&length=10
+https://mc2method.org/white-noise/download.php?file=39-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=40-Motor&length=10
+https://mc2method.org/white-noise/download.php?file=41-Ocean&length=10
+https://mc2method.org/white-noise/download.php?file=42-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=43-Stream&length=10
+https://mc2method.org/white-noise/download.php?file=44-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=45-Waves&length=10
+https://mc2method.org/white-noise/download.php?file=46-Underwater&length=10
+https://mc2method.org/white-noise/download.php?file=47-Waves&length=10
+https://mc2method.org/white-noise/download.php?file=48-Rain&length=10
+https://mc2method.org/white-noise/download.php?file=49-Underwater&length=10
+https://mc2method.org/white-noise/download.php?file=50-White-Noise&length=10
+https://mc2method.org/white-noise/download.php?file=51-Train&length=10
