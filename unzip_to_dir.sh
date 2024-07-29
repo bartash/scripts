8673bb2b9d582a8a9321be49a03a80dfc80aa6e0
Andrew Sherman <asherman@cloudera.com>
2024-07-29 13:05:21 -0700

    make it more verbose about if zip is removed or not

--- unzip_to_dir.sh
+++ unzip_to_dir.sh
@@ -17,17 +17,24 @@ then
   dir=$(echo $NAME | sed 's/\.zip//')
   mkdir $dir
   cd $dir || exit 1
-  unzip  ../$NAME  && rm ../$NAME
+  unzip  ../$NAME
   # note unzip can return 1 if there were warnings, e.g. warning:  stripped absolute path spec 
 elif [[ $NAME == *.tar.gz ]]
 then
   dir=$(echo $NAME | sed 's/\.tar.gz//')
   mkdir $dir
   cd $dir || exit 1
-  tar xvzf ../$NAME && rm ../$NAME
+  tar xvzf ../$NAME
 else
    echo "file $NAME does not have a supported extension"
 fi
+if [[ $? -eq 0 ]]; then
+	echo "exit code was 0, remove $NAME"
+	rm ../$NAME
+else
+	echo "exit code was $? leaving $NAME"
+fi
+
 
 echo
 echo "target dir was $dir"
