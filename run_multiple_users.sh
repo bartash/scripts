2477511dd1b349ab3338fb8b6fb2b6847d93106f
Andrew Sherman <asherman@cloudera.com>
2024-10-09 11:05:45 -0700

    Run queries in impala-shell with many different users

---
+++ run_multiple_users.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Run queries in impala-shell with many different users
+
+# Start cluster with
+# start-impala-cluster.py --impalad_args="-fair_scheduler_allocation_path=fe/src/test/resources/fair-scheduler-test2.xml
+# -llama_site_path=fe/src/test/resources/llama-site-test2.xml"
+
+# Number of concurrent users
+TOTAL_USERS=4
+if [ "$#" -eq 1 ]
+then
+	TOTAL_USERS=$1
+fi
+
+QUERY='set REQUEST_POOL=queueE; select * from hoo where a <  sleep (30000);'
+
+for user_number in $(seq 1 ${TOTAL_USERS})
+do
+  IMPALA_USER="user-${user_number}"
+  echo "user is ${IMPALA_USER}"
+  impala-shell.sh -u ${IMPALA_USER}  -i localhost:21050 -q "${QUERY}" &
+done
