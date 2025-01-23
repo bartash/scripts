27753c81cffbaff52da93038f0c3659bd804fc80
Andrew Sherman <asherman@cloudera.com>
2025-01-23 14:11:22 -0800

    turn on enable_workload_mgmt

--- start-impala-admission-control.sh
+++ start-impala-admission-control.sh
@@ -12,7 +12,9 @@ LLAMA=llama-site-test2.xml
 
 ${IMPALA_HOME}/bin/start-impala-cluster.py \
  --cluster_size=3 --num_coordinators=3 --log_dir=/tmp/ --log_level=1 \
+    --catalogd_args="--enable_workload_mgmt=true" \
  --impalad_args="-fair_scheduler_allocation_path ${IMPALA_HOME}/fe/src/test/resources/${FAIR} \
+    --enable_workload_mgmt=true \
     -llama_site_path ${IMPALA_HOME}/fe/src/test/resources/${LLAMA}" \
  --state_store_args="--statestore_update_frequency_ms=50   --statestore_priority_update_frequency_ms=50 \
     --statestore_heartbeat_frequency_ms=50"
