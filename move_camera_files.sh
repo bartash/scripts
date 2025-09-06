5edfcb401a6d39d9945f30c722a6f0d0c034d335
Andrew Sherman <andrew@ohair-sherman.com>
2025-09-06 08:25:16 -0700

    Make script work with Pixel camera as well as original samsung.

--- move_camera_files.sh
+++ move_camera_files.sh
@@ -24,10 +24,26 @@ fi
 
 for file in $(ls)
 do
+	case "$file" in
+  		2*)
+    			echo "Filename $file is from ALO camera"
+			year_cut="1-4"
+			month_cut="5-6"
+    			;;
+  		PXL_*)
+    			echo "Filename $file is from APS camera"
+			year_cut="5-8"
+			month_cut="9-10"
+    			;;
+  		*)
+    			echo "Filename $file has an unknown prefix"
+			exit 1
+    			;;
+	esac
 	# echo "file=$file"
-	year=$(echo $file | cut -c 1-4)
+	year=$(echo $file | cut -c $year_cut)
 	# echo "year=$year"
-	month=$(echo $file | cut -c 5-6)
+	month=$(echo $file | cut -c $month_cut)
 	# 10# prefix forces months like 09 to not be octal
 	month_name=$(echo ${MONTHS[10#$month]}-${year})
 	# echo "month=$month or $month_name"
