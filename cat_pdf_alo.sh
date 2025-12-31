2a5748cb6a9c14affa95e765b5bad0cb88ddaff9
Andrew Sherman <andrew@ohair-sherman.com>
2025-12-30 16:44:25 -0800

    remove extra output files

--- cat_pdf_alo.sh
+++ cat_pdf_alo.sh
@@ -4,9 +4,6 @@
 #
 set -o errexit
 OUTPUT=Revised_Walks_book_OHAIR_2026.pdf
-OUTPUT1=ONE_Walks_book_OHAIR_2026.pdf
-OUTPUT2=TWO_Walks_book_OHAIR_2026.pdf
-OUTPUT3=THREE_Walks_book_OHAIR_2026.pdf
 
 PDF_DIRECTORY=/mnt/g/data/Photos-Amy/432Flood-project/0-Sunnyside/05-Walks-Book/0_REMAKE_2/FINAL_03_PDFS
 
@@ -23,15 +20,4 @@ echo running pdfunite [0-9]*.pdf $OUTPUT
 pdfunite [0-9]*.pdf $OUTPUT
 echo "Merged file is ${OUTPUT}."
 
-LIST1=$(ls 0[1-7]*.pdf)
-echo "building ONE ${OUTPUT1} from ${LIST1}"
-pdfunite ${LIST1} ${OUTPUT1}
-
-LIST2=$(ls  0[8-9]*.pdf 1[0-3]*.pdf)
-echo "building TWO ${OUTPUT2} from ${LIST2}"
-pdfunite ${LIST2} ${OUTPUT2}
-
-LIST3=$(ls 1[4-9]*.pdf)
-echo "building THREE ${OUTPUT3} from ${LIST3}"
-pdfunite ${LIST3} ${OUTPUT3}
 
