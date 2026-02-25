5c5d6674128d1486dffb796eed1f3b3c76ba90fc
Andrew Sherman <andrew@ohair-sherman.com>
2026-02-25 08:53:39 -0800

    start of new external backup

---
+++ backup_external_cyg.sh
@@ -0,0 +1,30 @@
+(set -o igncr) 2>/dev/null && set -o igncr; # comment is needed
+
+cd /cygdrive
+echo backup_external ...
+
+SOURCE=h
+BACK=e
+
+
+if [ ! -d ${BACK} ]; then
+	echo Could not find ${BACK} volume
+	exit 1
+else
+	echo found ${BACK}
+fi
+if [ ! -d ${BACK}/backup ]; then
+	echo "Could not find ${BACK}/backup, maybe wrong drive mounted"
+	exit 1
+else
+	echo found ${BACK}
+fi
+if [ ! -d ${SOURCE} ]; then
+	echo Could not find ${SOURCE} volume
+	exit 1
+else
+	echo found ${SOURCE}
+fi
+
+rsync -av --exclude '*.lnk'  --exclude '.canary' --delete  --no-owner --no-group   $SOURCE/* $BACK/backup | ~/scripts/delete_dir_lines_from_rsync
+rsync -av --exclude '*.lnk'  --exclude '.canary' --delete  --no-owner --no-group   $SOURCE/data $BACK/backup | ~/scripts/delete_dir_lines_from_rsync
