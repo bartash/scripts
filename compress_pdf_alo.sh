2886e95f6ba7d1d778330c5cadc854a47bfdf3de
Andrew Sherman <andrew@ohair-sherman.com>
2026-02-28 08:30:25 -0800

    more compression

--- compress_pdf_alo.sh
+++ compress_pdf_alo.sh
@@ -4,6 +4,19 @@
 # /printer: High quality (300 dpi). Best if you still plan to print the document.
 # /prepress: Highest quality (300 dpi, preserves color). Use this if color accuracy is your #1 priority
 INPUT=$1
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=printer_$INPUT $INPUT
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=ebook_$INPUT $INPUT
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=screen_$INPUT $INPUT
+# gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=printer_$INPUT $INPUT
+# gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=ebook_$INPUT $INPUT
+# gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=screen_$INPUT $INPUT
+
+
+gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH \
+   -dColorImageFilter=/DCTEncode -dColorImageResolution=150 \
+   -dGrayImageFilter=/DCTEncode -dGrayImageResolution=150 \
+   -dMonoImageFilter=/CCITTFaxEncode -dMonoImageResolution=300 \
+   -sOutputFile=150_$INPUT $INPUT
+
+gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH \
+   -dColorImageFilter=/DCTEncode -dColorImageResolution=100 \
+   -dGrayImageFilter=/DCTEncode -dGrayImageResolution=100 \
+   -dMonoImageFilter=/CCITTFaxEncode -dMonoImageResolution=300 \
+   -sOutputFile=100_$INPUT $INPUT
