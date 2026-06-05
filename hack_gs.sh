71dd323264fd14c8fec7851c611fbd8431efdd8e
Andrew Sherman <andrew@ohair-sherman.com>
2026-06-05 14:14:05 -0700

    hacking at using pdftops

---
+++ hack_gs.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# concatenate pdfs
+#
+# possible blackmagic, I also did
+#  sudo apt-get install fonts-sil-gentiumplus
+#  sudo apt install fonts-croscore
+#
+set -o errexit
+OUTPUT=Revised_Walks_book_OHAIR_2026.pdf
+INPUT=RGB_Revised_Walks_book_OHAIR_2026.pdf
+
+PDF_DIRECTORY=/mnt/c/Users/ac9/tmp/New_PDFs_4
+
+# Check if the specified directory exists.
+if [ ! -d "$PDF_DIRECTORY" ]; then
+    echo "Error: Directory '$PDF_DIRECTORY' not found."
+    exit 1
+fi
+
+cd ${PDF_DIRECTORY}
+
+pdftops $INPUT temporary.ps
+
+# https://gemini.google.com/app/c44b6ec45425466b
+# Note gs will not see user-specific fonts in %APPDATA%/...
+gs -o $OUTPUT \
+   -sDEVICE=pdfwrite \
+   -dPDFSETTINGS=/prepress \
+   -sColorConversionStrategy=CMYK \
+   -dProcessColorModel=/DeviceCMYK \
+   -dEncodeColorImages=true \
+   -dDownsampleColorImages=false \
+   -dCompatibilityLevel=1.4 \
+   -dEmbedAllFonts=true \
+   -dSubsetFonts=false \
+   -dCannotEmbedFontPolicy=/Error \
+   -sFONTPATH="/mnt/c/Windows/Fonts" \
+   temporary.ps
+
+
+
+
